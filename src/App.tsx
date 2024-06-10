e3863a7d8951395fbc9806d7eb11c5676cdae8c8
Jan Etschel <jan.etschel@icloud.com>
2024-06-10 10:05:25 +0200

    Added styled-components to do inline CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
+import styled from 'styled-components';
 
 const App = () => {
   return (
-    <div className="App">
+    <Main className="App">
       Hello World
-    </div>
+    </Main>
   );
 }
 
 export default App;
+
+const Main = styled.div`
+  text-align: center;
+`;
